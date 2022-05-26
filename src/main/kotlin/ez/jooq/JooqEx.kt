c3c27c28d404e45691b04385ea5089cf466d1c9a
huangwl <410488219@qq.com>
2022-05-26 17:14:53 +0800

    remove unnecessary fun

--- src/main/kotlin/ez/jooq/JooqEx.kt
+++ src/main/kotlin/ez/jooq/JooqEx.kt
@@ -1,25 +1,12 @@
 package ez.jooq
 
-import org.jooq.*
-import java.math.BigDecimal
-import java.math.RoundingMode
-import java.time.LocalDateTime
-import java.time.OffsetDateTime
-import java.time.ZoneId
+import org.jooq.Configuration
+import org.jooq.Record
+import org.jooq.RecordMapper
 
 typealias JooqConf = Configuration
 
+@Suppress("unused")
 fun <RECORD : Record, POJO : Any> RECORD.mapBy(mapper: RecordMapper<RECORD, POJO>): POJO? {
   return mapper.map(this)
-}
-
-/**
- * 分转成元（默认保留两位小数，四舍六入五成双）
- */
-fun Long.asFenToYuan(scale: Int = 2): BigDecimal {
-  return (BigDecimal(this).divide(BigDecimal(100), scale, RoundingMode.HALF_EVEN))
-}
-
-fun LocalDateTime.toOffsetDateTime(zoneId: ZoneId = ZoneId.systemDefault()): OffsetDateTime {
-  return atZone(zoneId).toOffsetDateTime()
 }
