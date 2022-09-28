17831c7a0328813578d61cc2d98b26251fbbe601
huangwl <410488219@qq.com>
2022-09-28 10:24:55 +0800

    add extension for Attachable

--- src/main/kotlin/ez/jooq/JooqEx.kt
+++ src/main/kotlin/ez/jooq/JooqEx.kt
@@ -1,12 +1,16 @@
+@file:Suppress("unused")
+
 package ez.jooq
 
+import org.jooq.Attachable
 import org.jooq.Configuration
 import org.jooq.Record
 import org.jooq.RecordMapper
 
 typealias JooqConf = Configuration
 
-@Suppress("unused")
 fun <RECORD : Record, POJO : Any> RECORD.mapBy(mapper: RecordMapper<RECORD, POJO>): POJO? {
   return mapper.map(this)
 }
+
+fun <A: Attachable> A.attach(jooq: Jooq) = apply { attach(jooq.context().configuration()) }
