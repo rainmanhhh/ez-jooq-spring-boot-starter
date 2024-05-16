030f606a565458fbf77d7d38e3d4f6a13fdd67b5
huangwl <410488219@qq.com>
2024-05-16 09:28:00 +0800

    feat: add paginate function

--- src/main/kotlin/ez/jooq/JooqEx.kt
+++ src/main/kotlin/ez/jooq/JooqEx.kt
@@ -2,10 +2,7 @@
 
 package ez.jooq
 
-import org.jooq.Attachable
-import org.jooq.Configuration
-import org.jooq.Record
-import org.jooq.RecordMapper
+import org.jooq.*
 
 typealias JooqConf = Configuration
 
@@ -15,3 +12,25 @@ fun <RECORD : Record?, POJO : Any> RECORD.mapBy(mapper: RecordMapper<RECORD, POJ
 
 fun <A : Attachable?> (A & Any).attach(jooq: Jooq) =
   apply { attach(jooq.context().configuration()) }
+
+/**
+ * do pagination
+ * @param pageNo 1-based page number
+ * @param pageSize page size
+ * @param finalAction the final action to execute after pagination.
+ *   `this` is [SelectForUpdateStep], `it` is the count. example:
+ *   ```
+ *   {
+ *     Page(count = it, list = fetchInto(MyPojo::class.java))
+ *   }
+ *   ```
+ */
+fun <R : Record, Result> SelectLimitStep<R>.paginate(
+  pageNo: Int,
+  pageSize: Int,
+  finalAction: SelectForUpdateStep<R>.(Long) -> Result
+): Result {
+  val ctx = configuration() ?: throw RuntimeException("can't paginate on sql without jooq context")
+  val count = ctx.dsl().selectCount().from(this).fetchOneInto(Long::class.java)!!
+  return limit(pageSize).offset((pageNo - 1) * pageSize).finalAction(count)
+}
