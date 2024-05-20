9b801c64b99445841842294bc27768c61af00fe2
huangwl <410488219@qq.com>
2024-05-20 08:20:46 +0800

    feat: read jooq config settings from application-*.yml

--- src/main/kotlin/ez/jooq/EzJooqAutoConfig.kt
+++ src/main/kotlin/ez/jooq/EzJooqAutoConfig.kt
@@ -1,21 +1,32 @@
 package ez.jooq
 
+import org.jooq.conf.Settings
 import org.springframework.boot.autoconfigure.AutoConfigureAfter
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.boot.autoconfigure.jooq.DefaultConfigurationCustomizer
 import org.springframework.boot.autoconfigure.jooq.JooqAutoConfiguration
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.NestedConfigurationProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Import
 
-@Import(JooqAutoConfiguration::class)
 @AutoConfigureAfter(JooqAutoConfiguration::class)
+@ConditionalOnBean(JooqConf::class)
+@ConfigurationProperties("ez.jooq")
 @Configuration
 class EzJooqAutoConfig {
-  @ConditionalOnBean(JooqConf::class)
+  @NestedConfigurationProperty
+  var settings = Settings()
+
   @ConditionalOnMissingBean(Jooq::class)
   @Bean
   fun jooq(jooqConf: JooqConf): Jooq {
     return Jooq(jooqConf)
+  }
+
+  @Bean
+  fun jooqSettings() = DefaultConfigurationCustomizer {
+    it.setSettings(settings)
   }
 }
