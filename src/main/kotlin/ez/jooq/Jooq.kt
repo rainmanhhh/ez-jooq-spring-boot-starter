7d8f41a1252b784d41489499bbfbbef5799fa335
huangwl <410488219@qq.com>
2024-07-04 17:48:44 +0800

    refactor: use context instead of dsl

--- src/main/kotlin/ez/jooq/Jooq.kt
+++ src/main/kotlin/ez/jooq/Jooq.kt
@@ -2,30 +2,34 @@ package ez.jooq
 
 import org.jooq.*
 
-@Suppress("unused")
-class Jooq(private val dsl: () -> DSLContext) {
+@Suppress("unused", "MemberVisibilityCanBePrivate")
+class Jooq(
+  /**
+   * dsl context
+   */
+  val context: () -> DSLContext
+) {
   constructor(conf: JooqConf) : this(conf::dsl)
 
-  fun context() = dsl()
+  fun <R : Record> newRecord(t: Table<R>) = context().newRecord(t)
 
-  fun <R : Record> newRecord(t: Table<R>) = dsl().newRecord(t)
+  fun <R : Record> selectFrom(t: Table<R>): JooqWhereStep<R> =
+    JooqWhereStep(context().selectFrom(t))
 
-  fun <R : Record> selectFrom(t: Table<R>): JooqWhereStep<R> = JooqWhereStep(dsl().selectFrom(t))
+  fun <R : Record> insertInto(t: Table<R>): InsertSetStep<R> = context().insertInto(t)
 
-  fun <R : Record> insertInto(t: Table<R>): InsertSetStep<R> = dsl().insertInto(t)
+  fun <R : Record> update(t: Table<R>): UpdateSetFirstStep<R> = context().update(t)
 
-  fun <R : Record> update(t: Table<R>): UpdateSetFirstStep<R> = dsl().update(t)
-
-  fun <R : Record> deleteFrom(t: Table<R>): DeleteUsingStep<R> = dsl().deleteFrom(t)
+  fun <R : Record> deleteFrom(t: Table<R>): DeleteUsingStep<R> = context().deleteFrom(t)
 
   fun tx(transactional: (jooq: Jooq) -> Unit) =
-    dsl().transaction(TransactionalRunnable {
+    context().transaction(TransactionalRunnable {
       val j = Jooq(it)
       transactional(j)
     })
 
   fun <T> txResult(transactional: (jooq: Jooq) -> T): T =
-    dsl().transactionResult(TransactionalCallable {
+    context().transactionResult(TransactionalCallable {
       val j = Jooq(it)
       transactional(j)
     })
